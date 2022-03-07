c4821c5d58c96f998f9c3c4c9f6d2ab6c8b250f9
Andry <91080989+andreiartsiomenka@users.noreply.github.com>
2022-03-07 12:47:31 +0300

    Update task7.java

--- src/by/basic/unit1_Basics_of_software_code_development/loop/task7.java
+++ src/by/basic/unit1_Basics_of_software_code_development/loop/task7.java
@@ -12,25 +12,13 @@ import com.sun.security.auth.NTDomainPrincipal;
 public class task7 {
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
-		//Проверка 
-		
+		Scanner scanner = new Scanner(System.in); 
 		System.out.println("m >> ");
 		int m = scanner.nextInt();
 		System.out.println("n >> ");
 		int n = scanner.nextInt();
-		    //a,b,n,m,i: word;<br>
-		 //<br>
-		//begin<br>
-		    //write('Числовой промежуток: ');<br>
-		
-		    //readln(a,b);<br>
-		System.out.println("The number of dividers is not less than ...");
-		    //write('Количество делителей не менее... ');<br>
-		    //readln(n);<br>
+		System.out.println("The number of dividers is not less than ...(Количество делителей не менее...)");
 		int number = scanner.nextInt();
-		
-		    //while a <= b do begin<br>
 		while (m<n) {
 			int mod = 0;
 			for (int i = 0;i<m;i++) {
@@ -50,17 +38,6 @@ public class task7 {
 			}
 			m+=1;
 		}
-		        //m := 0;<br>
-		        //for i:=1 to a do<br>
-		            //if a mod i = 0 then m := m + 1;<br>
-		        //if m >= n then begin<br>
-		            //write(a,' - ', m,' - ');<br>
-		            //for i:=1 to a do<br>
-		                //if a mod i = 0 then write(i,' ');<br>
-		            //writeln;<br>
-		        //end;<br>
-		        //a := a + 1;<br>
 		    
-		
-	}
+		}
 }
